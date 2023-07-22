8764d5548099f44fda4f64c228105266055c3759
Manon Mucchielli <manonmucchielli@hotmail.fr>
2023-07-22 20:35:51 +0200

    [api] ajout function async fetch getDataGithub

---
+++ github-app/src/services/api/script.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+//import loupe from '../assets/icon-search.svg';
+
+
+export function Api(){
+    const [searchData, setSearchData] = useState({userName:"octocat"});
+    const [data, setData] = useState({});
+    
+    useEffect(() => {
+        async function fetchDataGit(){
+            try{
+                // destructuration de l'objet
+                const response = await fetch(`https://api.github.com/users/${searchData.userName}`);
+                const data = await response.json();
+                setData(data);
+                console.log(data);
+                console.log(response);
+            } catch (error){
+               // console.error(error);
+            }
+        }
+        fetchDataGit();
+        // permet l'ajout de la valeur au tableau de dependance
+    }, [searchData.userName]);
+
+    return data;
+}
+
+
+   
