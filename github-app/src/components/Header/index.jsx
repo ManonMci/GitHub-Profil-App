92cbda2bdf4cc4e9585c10f57dcec0787627d7a7
Manon Mucchielli <manonmucchielli@hotmail.fr>
2023-07-24 09:20:44 +0200

    [feat] implémentation de la fonction du changement de theme

--- github-app/src/components/Header/index.jsx
+++ github-app/src/components/Header/index.jsx
@@ -1,18 +1,23 @@
+import { useContext } from 'react'
+import { ThemeContext } from '../../utils/context/DarkMode'
+
 import "../../components/Header/index.css";
 import moon from '../../assets/icon-moon.svg';
-
-//import { ThemeProvider } from './ThemeContext';
+import sun from '../../assets/icon-sun.svg';
 
 function Header(){
-    const onClick = {
-    }
-    
+    const { toggleTheme, theme } = useContext(ThemeContext)
+
     return (
         <div className="devFinder">
-            <p>devfinder</p>
-            <div className="Dark-mode">
-                <button className="dark-button">D A R K</button>
-                <img className="img-moon" src={moon}/>
+            <p className="p-devFinder">devfinder</p>
+            <div className="mode">
+                <button 
+                    className="mode-button" 
+                    onClick= {() => toggleTheme()}>
+                        {theme === 'light' ? <p className="dark-button">DARK</p> : <p className="light-button">LIGHT</p> } 
+                        {theme === 'light' ? <img className="img-moon" src={moon}/> : <img className="img-sun" src={sun}/> }
+                </button>
             </div>
         </div>
     );
